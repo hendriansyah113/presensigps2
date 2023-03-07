75c22b06f2b57d4412bd231eb25d26f6a7d027e1
Hendriansyah <hendriansyah113@gmail.com>
2023-03-07 21:38:31 +0700

    Edit & Delete Data Karyawan

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Redirect;
 
 class KaryawanController extends Controller
@@ -25,7 +26,7 @@ class KaryawanController extends Controller
             $query->where('karyawan.kode_dept', 'like', '%' . $request->kode_dept . '%');
         }
 
-        $karyawan = $query->paginate(2);
+        $karyawan = $query->paginate(10);
 
         $departemen = DB::table('departemen')->get();
         return view('karyawan.index', compact('karyawan', 'departemen'));
@@ -111,6 +112,61 @@ class KaryawanController extends Controller
             }
         }catch(\Exception $e){
             return Redirect::back()->with(['warning' => 'Data Gagal Disimpan']);
+        }
+     }
+
+     public function edit(Request $request)
+     {
+        $nik = $request->nik;
+        $departemen = DB::table('departemen')->get();
+        $karyawan = DB::table('karyawan')->where('nik', $nik)->first();
+        return view('karyawan.edit', compact('departemen', 'karyawan'));
+     }
+
+     public function updatepanel($nik, Request $request)
+     {
+        $nik = $request->nik;
+        $nama_lengkap = $request->nama_lengkap;
+        $jabatan = $request->jabatan;
+        $no_hp = $request->no_hp;
+        $kode_dept = $request->kode_dept;
+        $old_foto = $request->old_foto;
+        if($request->hasFile('foto')){
+            $foto = $nik . "." . $request->file('foto')->getClientOriginalExtension();
+        }else{
+            $foto = $old_foto;
+        }
+
+        try{
+            $data = [
+                'nama_lengkap' => $nama_lengkap,
+                'jabatan' => $jabatan,
+                'no_hp' => $no_hp,
+                'kode_dept' => $kode_dept,
+                'foto' => $foto,
+            ];
+            $update = DB::table('karyawan')->where('nik', $nik)->update($data);
+            if($update){
+                if($request->hasFile('foto')){
+                    $folderPath = "public/uploads/karyawan/";
+                    $folderPathOld = "public/uploads/karyawan/" . $old_foto;
+                    Storage::delete($folderPathOld);
+                    $request->file('foto')->storeAs($folderPath, $foto);
+                }
+                return Redirect::back()->with(['success' => 'Data Berhasil Diupdate']);
+            }
+        }catch(\Exception $e){
+            return Redirect::back()->with(['warning' => 'Data Gagal Diupdate']);
+        }
+     }
+
+     public function delete($nik)
+     {
+        $delete = DB::table('karyawan')->where('nik', $nik)->delete();
+        if($delete){
+            return Redirect::back()->with(['success' => 'Data Berhasil Dihapus']);
+        }else{
+            return Redirect::back()->with(['warning' => 'Data Gagal Dihapus']);
         }
      }
 }
--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -125,7 +125,49 @@
                                                         @endif
                                                     </td>
                                                     <td>{{ $d->nama_dept }}</td>
-                                                    <td></td>
+                                                    <td>
+                                                        <div class="btn-group">
+                                                            <a href="#" class="edit btn btn-info btn-sm"
+                                                                nik="{{ $d->nik }}">
+                                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                                    class="icon icon-tabler icon-tabler-edit" width="40"
+                                                                    height="40" viewBox="0 0 24 24" stroke-width="2"
+                                                                    stroke="currentColor" fill="none"
+                                                                    stroke-linecap="round" stroke-linejoin="round">
+                                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none">
+                                                                    </path>
+                                                                    <path
+                                                                        d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1">
+                                                                    </path>
+                                                                    <path
+                                                                        d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z">
+                                                                    </path>
+                                                                    <path d="M16 5l3 3"></path>
+                                                                </svg>
+                                                            </a>
+                                                            <form action="/karyawan/{{ $d->nik }}/delete"
+                                                                method="POST" style="margin-left: 5px">
+                                                                @csrf
+                                                                <a class="delete-confirm btn btn-danger btn-sm">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                                                        class="icon icon-tabler icon-tabler-trash-filled"
+                                                                        width="40" height="40" viewBox="0 0 24 24"
+                                                                        stroke-width="2" stroke="currentColor"
+                                                                        fill="none" stroke-linecap="round"
+                                                                        stroke-linejoin="round">
+                                                                        <path stroke="none" d="M0 0h24v24H0z"
+                                                                            fill="none"></path>
+                                                                        <path
+                                                                            d="M20 6a1 1 0 0 1 .117 1.993l-.117 .007h-.081l-.919 11a3 3 0 0 1 -2.824 2.995l-.176 .005h-8c-1.598 0 -2.904 -1.249 -2.992 -2.75l-.005 -.167l-.923 -11.083h-.08a1 1 0 0 1 -.117 -1.993l.117 -.007h16z"
+                                                                            stroke-width="0" fill="currentColor"></path>
+                                                                        <path
+                                                                            d="M14 2a2 2 0 0 1 2 2a1 1 0 0 1 -1.993 .117l-.007 -.117h-4l-.007 .117a1 1 0 0 1 -1.993 -.117a2 2 0 0 1 1.85 -1.995l.15 -.005h4z"
+                                                                            stroke-width="0" fill="currentColor"></path>
+                                                                    </svg>
+                                                                </a>
+                                                            </form>
+                                                        </div>
+                                                    </td>
                                                 </tr>
                                             @endforeach
                                         </tbody>
@@ -274,6 +316,20 @@
             </div>
         </div>
     </div>
+    {{-- Modal Edit --}}
+    <div class="modal modal-blur fade" id="modal-editkaryawan" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Edit Data Karyawan</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body" id="loadeditform">
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @push('myscript')
@@ -282,6 +338,46 @@
             $("#btnTambahkaryawan").click(function() {
                 $("#modal-inputkaryawan").modal("show");
             });
+
+            $(".edit").click(function() {
+                var nik = $(this).attr('nik');
+                $.ajax({
+                    type: 'POST',
+                    url: '/karyawan/edit',
+                    chace: false,
+                    data: {
+                        _token: "{{ csrf_token() }}",
+                        nik: nik
+                    },
+                    success: function(respond) {
+                        $("#loadeditform").html(respond);
+                    }
+                });
+                $("#modal-editkaryawan").modal("show");
+            });
+
+            $(".delete-confirm").click(function(e) {
+                var form = $(this).closest('form');
+                e.preventDefault();
+                Swal.fire({
+                    title: 'Anda Yakin?',
+                    text: "Data Yang Sudah Dihapus Tidak Bisa Dikembalikan!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Ya, Hapus'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        form.submit();
+                        Swal.fire(
+                            'Dihapus!',
+                            'Data Telah Berhasil Dihapus',
+                            'success'
+                        )
+                    }
+                })
+            })
 
             $("#frmKaryawan").submit(function() {
                 var nik = $("#nik").val();
